85f82e43c0be837f82a82e9d5e0e3c287347be05
madufek547 <69473923+madufek547@users.noreply.github.com>
2023-03-01 11:27:13 -0500

    the beginning

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+import openai
+import os
+app = Flask(__name__)
+
+
+
+@app.route('/campaign', methods=['GET', 'POST'])
+def campaign():
+    if request.method == 'POST':
+        campaign_name = request.form['campaign_name']
+        num_players = request.form['num_players']
+        format = request.form['format']
+        length = request.form['length']
+        campaign_type = request.form['type']
+        return redirect(url_for('confirm', campaign_name=campaign_name, num_players=num_players, format=format, length=length))
+    return render_template('campaign.html')
+
+@app.route('/confirm', methods=['GET', 'POST'])
+def confirm():
+    if request.method == 'POST':
+        campaign_name = request.form['campaign_name']
+        num_players = request.form['num_players']
+        length = request.form['length']
+        campaign_type = request.form['type']
+        format = request.form['format']
+        prompt = generate_prompt(campaign_name, num_players, format, length, campaign_type)
+        campaign = generate_campaign(prompt)
+        return render_template('confirm.html', prompt=prompt, campaign=campaign)
+    else:
+        return redirect(url_for('campaign'))
+
+
+
+openai.api_key= "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"
+
+def generate_campaign(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=3900,
+        n=1,
+        stop=None,
+        temperature=1,
+    )
+
+    campaign = response.choices[0].text.strip()
+    return campaign
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def generate_prompt(campaign_name, num_players, format, length, campaign_type):
+    prompt = f"Create a {format} DND/Pathfinder-like campaign called '{campaign_name}' for {num_players} players in which each session(s) lasts {length} hours. The campaign should include a variety of encounters and NPCs for the players to interact with and follow the theme given. This is the description given by the DM:'{campaign_type}."
+    return prompt
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
