361c363df0946d515df3d6ffce0e62e7640b2ed0
madufek547 <69473923+madufek547@users.noreply.github.com>
2023-03-02 21:23:33 -0500

    added jquery and socketio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response, send_file
 import requests
 import openai
 import os
@@ -7,7 +7,12 @@ from dotenv import load_dotenv
 import dotenv
 import func.dice as dice
 import random
+import func.character as character
+from flask_socketio import SocketIO, emit
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 
 load_dotenv(override=True)
 api_key = dotenv.get_key(".env", "OPENAI_API_KEY")
@@ -20,6 +25,33 @@ ABILITIES = {
     'wis': 'Wisdom',
     'cha': 'Charisma'
 }
+
+def css_styles():
+    css_file = open('static/css/styles.css', 'r')
+    content = css_file.read()
+    css_file.close()
+    return Response(content, mimetype='text/css')
+
+@app.route('/static/js/jquery-3.6.3.min.js')
+def jquery():
+    js_file = open('static/js/jquery-3.6.3.min.js', 'r')
+    content = js_file.read()
+    js_file.close()
+    return Response(content, mimetype='application/javascript')
+
+@app.route('/static/js/socket.io.min.js')
+def socketio():
+    js_file = open('static/js/socket.io.min.js', 'r')
+    content = js_file.read()
+    js_file.close()
+    return Response(content, mimetype='application/javascript')
+
+@app.route('/static/js/socket.io.min.js.map')
+def map():
+    js_file = open('static/js/socket.io.min.js.map', 'r')
+    content = js_file.read()
+    js_file.close()
+    return Response(content, mimetype='application/javascript')
 
 def calculate_modifier(ability_scores, ability):
     if ability not in ability_scores:
@@ -65,9 +97,17 @@ def roll_ability_scores():
     print(f"Generated ability scores: {ability_scores}")
     return ability_scores
 
+@app.route('/styles.css')
+def send_css():
+    return send_file('path/to/styles.css', mimetype='text/css')
 
-
-
+@app.route('/static/js/chat.js')
+def chat_js():
+    js_file = open('static/js/chat.js', 'rb')
+    content = js_file.read()
+    js_file.close()
+    response = Response(content, mimetype='text/javascript')
+    return response
 
 @app.route('/campaign', methods=['GET', 'POST'])
 def campaign():
@@ -77,7 +117,10 @@ def campaign():
         format = request.form['format']
         length = request.form['length']
         campaign_type = request.form['type']
-        return redirect(url_for('confirm', campaign_name=campaign_name, num_players=num_players, format=format, length=length))
+            # Handle confirm button click
+        prompt = generate_prompt(campaign_name, num_players, format, length, campaign_type)
+        campaign = generate_campaign(prompt)
+        return render_template('confirm.html', prompt=prompt, campaign=campaign)
     return render_template('campaign.html')
 
 @app.route('/confirm', methods=['GET', 'POST'])
@@ -93,6 +136,11 @@ def confirm():
         return render_template('confirm.html', prompt=prompt, campaign=campaign)
     else:
         return redirect(url_for('campaign'))
+
+@app.route('/lobby', methods=['GET', 'POST'])
+def lobby():
+    return render_template('lobby.html')
+
 
 @app.route('/charactersheet', methods=['GET', 'POST'])
 def character_sheet():
@@ -118,8 +166,11 @@ def character_sheet():
 
         hp = sum(hit_dice_value) + con_mod
 
+        #playerCharacter = character.Character(name, race, char_class, age, gear, weapons, spells, ability_scores, hp, extra_info)
+        # Add character to list of characters
+        #characters.append(character)
         # Generate GPT response for background, motivation, and alignment
-        prompt = f"Generate a backstory, motivation, and alignment for a {race} {char_class} named {name}."
+        prompt = f"Generate a backstory, motivation, and alignment for a {race} {char_class} named {name}. This character is {age} years old, has {gear} for equipment, knows this list of spells{spells}, and if there is extra information from the player it will be found here: {extra_info}"
         response = generate_gpt_response(prompt)
 
         # Render character sheet template with generated data
@@ -149,7 +200,14 @@ def character_sheet():
     else:
         return render_template('charactersheet.html')
 
+@socketio.on('message')
+def handle_message(data):
+    room = data['room']
+    message = data['message']
+    sender = data['sender']
 
+    # Broadcast the message to all clients in the room
+    emit('chat_message', {'sender': sender, 'message': message}, room=room)
 
 
 def generate_campaign(prompt):
@@ -211,6 +269,8 @@ def confirmcharacter():
     return render_template('confirmcharacter.html', name=name, age=age, race=race, char_class=char_class, gear=gear, weapons=weapons, spells=spells, abilities=scores, modifiers=modifiers, hp=hp, response=response, extra_info=extra_info)
 
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -220,6 +280,24 @@ def generate_prompt(campaign_name, num_players, format, length, campaign_type):
     prompt = f"Create a {format} DND/Pathfinder-like campaign called '{campaign_name}' for {num_players} players in which each session(s) lasts {length} hours. The campaign should include a variety of encounters and NPCs for the players to interact with and follow the theme given. This is the description given by the DM:'{campaign_type}."
     return prompt
 
+@socketio.on('connect')
+def on_connect():
+    print('A user has connected')
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print('A user has disconnected')
+
+@socketio.on('message')
+def handle_message(data):
+    room = data['room']
+    message = data['message']
+    sender = data['sender']
+
+    # Broadcast the message to all clients in the room
+    emit('chat_message', {'sender': sender, 'message': message}, room=room)
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
